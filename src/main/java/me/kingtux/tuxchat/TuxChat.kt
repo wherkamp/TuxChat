604ad0d7e85b289f7fed1e6d66cb7a3e0594ed90
wherkamp <wherkamp@kingtux.me>
2018-09-03 18:25:04 -0400

    Stopped Triggering NickAC

--- src/main/java/me/kingtux/tuxchat/TuxChat.kt
+++ src/main/java/me/kingtux/tuxchat/TuxChat.kt
@@ -16,19 +16,13 @@ class TuxChat : JavaPlugin(), Listener {
 
   }
 
-  override fun onDisable() {
-    // Plugin shutdown logic
-  }
-
   @EventHandler
   fun chatEvent(event: AsyncPlayerChatEvent) {
-    event.isCancelled = true;
     var format: String = ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(event.player, config.getString("ChatFormat")))
 
-    var message = format.replace("{message}", event.message, true);
+    event.format = format.replace("{message}", "%2\$s", true);
     if (event.player.hasPermission("tuxchat.color")) {
-      message = ChatColor.translateAlternateColorCodes('&', message);
+      event.message = ChatColor.translateAlternateColorCodes('&', event.message);
     }
-    Bukkit.getServer().broadcastMessage(message);
   }
 }
